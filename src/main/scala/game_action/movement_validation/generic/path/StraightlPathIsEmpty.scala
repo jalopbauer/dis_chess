2ba36da4667828fdd6020eff53437fdd9dbeb390
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-16 19:11:03 -0300

    Movement validation

--- src/main/scala/game_action/movement_validation/generic/path/StraightlPathIsEmpty.scala
+++ src/main/scala/game_action/movement_validation/generic/path/StraightlPathIsEmpty.scala
@@ -2,6 +2,7 @@ package game_action.movement_validation.generic.path:
 
   import game_action.movement_validation.MovementValidator
   import game_action.movement_validation.generic.ToPositionIsEmptyValidator
+  import game_action.movement_validation.generic.movement.MovementIsStraight
   import game_data.GameData
   import game_data.board.Coordinate
   import game_data.movement.Movement
@@ -10,10 +11,16 @@ package game_action.movement_validation.generic.path:
 
   case class StraightlPathIsEmpty() extends MovementValidator:
     val toPositionIsEmptyValidator: ToPositionIsEmptyValidator = ToPositionIsEmptyValidator()
+    val movementIsStraight: MovementIsStraight = MovementIsStraight()
     def act(gameData: GameData): Either[GameData, GameInterruption] =
-      val movement = gameData.movement
-      val from = movement.from
-      val to = movement.to
-      val vector = to.vector(from)
-      if((1 to vector.x).forall(i => toPositionIsEmptyValidator.act(GameData(gameData.board, Movement(from, Coordinate(to.x + i, to.y + i), movement.player), gameData.turns)).isRight)) return Left(gameData)
-      Right(MoveIsInvalid(gameData))
+      movementIsStraight.act(gameData) match
+        case Left(gameData) =>
+          val movement = gameData.movement
+          val to = movement.to
+          val from = movement.from
+          val vector = to.vector(from)
+          val maxValsAndFunctio = if (to.x == 0) (vector.y,(i: Int) => Coordinate(to.x, to.y + i))
+          else (vector.x, (i: Int) => Coordinate(to.x + i, to.y))
+          if ((1 to maxValsAndFunctio._1).forall(i => toPositionIsEmptyValidator.act(GameData(gameData.board, Movement(from, maxValsAndFunctio._2(i), movement.player), gameData.turns)).isRight)) Left(gameData)
+          else Right(MoveIsInvalid(gameData))
+        case Right(moveIsInvalid) => Right(moveIsInvalid)
