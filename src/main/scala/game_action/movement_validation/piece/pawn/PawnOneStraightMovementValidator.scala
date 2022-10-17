8ae07aeac77e56e9eb1308ff254e10a614b6f2f4
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-17 17:55:40 -0300

    Movement validation

--- src/main/scala/game_action/movement_validation/piece/pawn/PawnOneStraightMovementValidator.scala
+++ src/main/scala/game_action/movement_validation/piece/pawn/PawnOneStraightMovementValidator.scala
@@ -1,9 +1,9 @@
 package game_action.movement_validation.piece.pawn
 
-import game_action.movement_validation.generic.movement.{MovementIsDiagonalDownTopColorValidator, MovementIsDiagonalUpBottomColorValidator, MovementIsDiagonalValidator, MovementIsOneDiagonalUpValidator, MovementIsStraightDownTopColorValidator, MovementIsStraightUpBottomColorValidator}
-  import game_action.movement_validation.generic.{AndCompositeValidator, OrCompositeValidator, ToPositionIsEmptyOrPieceIsDifferentColorValidator}
-  import game_data.piece.PieceColor
+import game_action.movement_validation.generic.movement.{MovementIsDiagonalDownTopColorValidator, MovementIsDiagonalUpBottomColorValidator, MovementIsDiagonalValidator, MovementIsOneDiagonalUpValidator, MovementIsStraightDownTopColorValidator, MovementIsStraightUpBottomColorValidator, OneStraightRespectingColorValidator}
+import game_action.movement_validation.generic.{AndCompositeValidator, OrCompositeValidator, ToPositionIsEmptyOrPieceIsDifferentColorValidator, ToPositionIsEmptyValidator}
+import game_data.piece.PieceColor
 
   case class PawnOneStraightMovementValidator(bottomColor: PieceColor
-                                              , topColor: PieceColor) extends OrCompositeValidator(MovementIsStraightUpBottomColorValidator(bottomColor)
-                                                                            , MovementIsStraightDownTopColorValidator(topColor))
+                                              , topColor: PieceColor) extends AndCompositeValidator(ToPositionIsEmptyValidator()
+                                                                            , OneStraightRespectingColorValidator(bottomColor: PieceColor, topColor: PieceColor))
