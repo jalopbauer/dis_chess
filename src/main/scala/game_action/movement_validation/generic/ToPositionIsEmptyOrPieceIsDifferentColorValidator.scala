d495037db9152f07f1cdde653eb888e21751b403
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-17 11:39:59 -0300

    Movement validation

--- src/main/scala/game_action/movement_validation/generic/ToPositionIsEmptyOrPieceIsDifferentColorValidator.scala
+++ src/main/scala/game_action/movement_validation/generic/ToPositionIsEmptyOrPieceIsDifferentColorValidator.scala
@@ -4,13 +4,8 @@ package game_action.movement_validation.generic:
   import game_data.GameData
   import game_interruption.GameInterruption
   import game_interruption.movement_validation.MoveIsInvalid
+  case class ToPositionIsEmptyOrPieceIsDifferentColorValidator() extends OrCompositionValidator(ToPositionIsEmptyValidator()
+                                                                                                , ToPiecePositionIsDifferentColorValidator())
 
-  case class ToPositionIsEmptyOrPieceIsDifferentColorValidator() extends MovementValidator:
-    val toPositionIsEmptyValidator: ToPositionIsEmptyValidator = ToPositionIsEmptyValidator()
-    val toPiecePositionIsDifferentColorValidator: ToPiecePositionIsDifferentColorValidator = ToPiecePositionIsDifferentColorValidator()
-    def act(gameData: GameData): Either[GameData, GameInterruption] =
-      if (toPositionIsEmptyValidator.act(gameData).isLeft) Left(gameData)
-      else if(toPiecePositionIsDifferentColorValidator.act(gameData).isLeft) Left(gameData)
-      else Right(MoveIsInvalid(gameData))
 
 
