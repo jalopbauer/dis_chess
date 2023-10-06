634e3c84f58449db3c097a6a274e12e3f8439d37
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-10-06 12:33:31 -0300

    progress

--- lib/src/main/kotlin/dis_chess/chessPiece/King.kt
+++
@@ -1,5 +0,0 @@
-package dis_chess.chessPiece
-
-import dis_chess.chessPiece.colour.ChessPieceColour
-
-data class King(val chessPieceColour : ChessPieceColour) : ChessPiece
--- lib/src/main/kotlin/dis_chess/chessPiece/Pawn.kt
+++
@@ -1,5 +0,0 @@
-package dis_chess.chessPiece
-
-import dis_chess.chessPiece.colour.ChessPieceColour
-
-data class Pawn(val chessPieceColour : ChessPieceColour) : ChessPiece
