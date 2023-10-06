58b50051a85dcbc2db8ce7de266caf1cedfe8f63
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-10-06 13:33:40 -0300

    Clarer to string

--- lib/src/main/kotlin/dis_chess/position/RowColumnPosition.kt
+++ lib/src/main/kotlin/dis_chess/position/RowColumnPosition.kt
@@ -1,3 +1,7 @@
 package dis_chess.position
 
-data class RowColumnPosition(val row : Int, val column : Int) : Position
+data class RowColumnPosition(val row : Int, val column : Int) : Position {
+    override fun toString(): String {
+        return "($row, $column)"
+    }
+}
